b4f518e9675710a0556c3263c4ea74fd4a3f7c4f
조성준 (Seongjoon Cho) <h0meb0dysj@gmail.com>
2022-10-09 17:37:37 +0900

    Edit PORT in autoexec.py to constant

--- server_agent/autoexec.py
+++ server_agent/autoexec.py
@@ -1,10 +1,9 @@
 import os
 import time
-from server_agent import PORT
 
 while True:
     netstat = os.popen('netstat -ao').read()
-    if str(PORT) in netstat:  # if server agent is running
+    if str(12345) in netstat:  # if server agent is running
         pass
     else:
         os.system('python3 server_agent.py')
