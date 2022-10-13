0f8e6a1cc9160a23b2407fa3bb21fa2dba984005
조성준 (Seongjoon Cho) <h0meb0dysj@gmail.com>
2022-10-13 20:47:01 +0900

    Subordinate to server_agent.py

--- server_agent/autoexec.py
+++ server_agent/autoexec.py
@@ -1,9 +1,10 @@
 import os
 import time
+from server_agent import HOST, PORT
 
 while True:
     netstat = os.popen('netstat -ao').read()
-    if str(12345) in netstat:  # if server agent is running
+    if f'{HOST}:{PORT}' in netstat:  # if server agent is running
         pass
     else:
         os.system('python3 server_agent.py')
