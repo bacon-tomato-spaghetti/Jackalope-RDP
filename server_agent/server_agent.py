64d992f3db11563defc1a66d393dc4101908b586
조성준 (Seongjoon Cho) <h0meb0dysj@gmail.com>
2022-10-14 12:58:38 +0900

    Store samples in server (to reproduce crashes)

--- server_agent/server_agent.py
+++ server_agent/server_agent.py
@@ -1,10 +1,12 @@
 from socket import gethostbyname, gethostname, socket, AF_INET, SOCK_STREAM
 import time
+import datetime
 from wtsapi import *
 
 HOST = '127.0.0.2'
 PORT = 12345
 SIZE = 9999  # maximum buffer size
+sampleDir = './samples'
 
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.bind((HOST, PORT))
@@ -19,6 +21,11 @@ while True:
     dataSize = len(data)
     print(f'[+] {dataSize}bytes received')
     hexdump(data, len(data))
+
+    sampleFile = sampleDir + '/sample_' + datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+    f = open(sampleFile, 'wb')
+    f.write(data)
+    f.close()
 
     RDPServer = OpenServer(b'localhost')
     RDPConnected = True
