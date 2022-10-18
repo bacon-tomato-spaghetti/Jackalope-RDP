270184d08f1a07a120cdfd4feeb4e460574fd8a0
조성준 (Seongjoon Cho) <h0meb0dysj@gmail.com>
2022-10-18 14:10:50 +0900

    Not to save samples in server

--- server_agent/server_agent.py
+++ server_agent/server_agent.py
@@ -1,18 +1,10 @@
-from socket import gethostbyname, gethostname, socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM
 import time
-import datetime
 from wtsapi import *
 
 HOST = '127.0.0.2'
 PORT = 12345
 SIZE = 9999  # maximum buffer size
-sampleDir = '.\\samples'
-
-def saveSample(data):
-    sampleFile = sampleDir + '\\sample_' + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S-%f')
-    f = open(sampleFile, 'wb')
-    f.write(data)
-    f.close()
 
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.bind((HOST, PORT))
@@ -27,8 +19,6 @@ while True:
     dataSize = len(data)
     print(f'[+] {dataSize}bytes received')
     hexdump(data, len(data))
-
-    saveSample(data)
 
     RDPServer = OpenServer(b'localhost')
     RDPConnected = True
@@ -48,8 +38,6 @@ while True:
         dataSize = len(data)
         print(f'[+] {dataSize}bytes received')
         hexdump(data, len(data))
-
-        saveSample(data)
 
         if not VirtualChannelWrite(RDPSND, data):
             break
