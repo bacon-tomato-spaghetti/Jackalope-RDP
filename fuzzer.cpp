e2f85afbe348b8a114c094c5ec6e6cf2f8ef8412
조성준 (Seongjoon Cho) <h0meb0dysj@gmail.com>
2022-10-19 18:41:44 +0900

    Revert last commit

--- fuzzer.cpp
+++ fuzzer.cpp
@@ -949,15 +949,15 @@ void Fuzzer::HandleCrash(ThreadContext* tc, std::string crash_name) {
     std::string cur_inputs_dir = DirJoin(crash_inputs_dir, crash_name);
     CreateDirectory(cur_inputs_dir);
 
-    std::vector<std::pair<std::string, size_t>> list = dynamic_cast<TinyInstInstrumentation*>(tc->instrumentation)->ExportList();
+    auto list = dynamic_cast<TinyInstInstrumentation*>(tc->instrumentation)->ExportList();
     int idx = 1;
     std::string sample_name;
     FILE* fp = NULL;
 
-    for (std::vector<std::pair<std::string, size_t>>::iterator itr = list.begin(); itr != list.end(); itr++) {
+    for (auto itr = list.begin(); itr != list.end(); itr++) {
         sample_name = DirJoin(cur_inputs_dir, std::to_string(idx));
         fp = fopen(sample_name.c_str(), "wb");
-        fwrite(itr->first.c_str(), sizeof(char), itr->second, fp);
+        fwrite((*itr).first.c_str(), (*itr).second, 1, fp);
         fclose(fp);
         idx++;
     }
