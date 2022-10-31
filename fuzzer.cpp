a7b0936deb159e4688970fe3b956265f6e642f7f
조성준 (Seongjoon Cho) <h0meb0dysj@gmail.com>
2022-10-31 11:52:29 +0900

    Not try to reproduce crashes

--- fuzzer.cpp
+++ fuzzer.cpp
@@ -306,6 +306,7 @@ RunResult Fuzzer::RunSampleAndGetCoverage(ThreadContext *tc, Sample *sample, Cov
     {
         string crash_desc = tc->instrumentation->GetCrashName();
 
+        /*
         if (crash_reproduce_retries > 0)
         {
             if (TryReproduceCrash(tc, sample, init_timeout, timeout) == CRASH)
@@ -318,6 +319,7 @@ RunResult Fuzzer::RunSampleAndGetCoverage(ThreadContext *tc, Sample *sample, Cov
                 crash_desc = "flaky_" + crash_desc;
             }
         }
+        */
 
         bool should_save_crash = false;
         int duplicates = 0;
