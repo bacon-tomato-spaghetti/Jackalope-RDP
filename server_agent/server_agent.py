582e192feb960eca9f98b7bc8f808bee06c011d1
조성준 (Seongjoon Cho) <h0meb0dysj@gmail.com>
2022-11-04 13:36:16 +0900

    Fix server agent

--- server_agent/server_agent.py
+++ server_agent/server_agent.py
@@ -6,7 +6,7 @@ HOST = '127.0.0.2'
 PORT = 12345
 SIZE = 1600  # maximum buffer size
 
-VCHandleValid = False  # check if virtual channel handle is valid
+VCHandleValid = False
 
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.bind((HOST, PORT))
@@ -17,21 +17,20 @@ client_socket, client_addr = server_socket.accept()
 print(f'[+] Client addr: {client_addr}')
 
 while True:
-    RDPServer = OpenServer(b'localhost')
+    data = client_socket.recv(SIZE)
+    dataSize = len(data)
+    print(f'[+] {dataSize}bytes received')
+    hexdump(data, len(data))
 
-    while True:
-        RDPSND = VirtualChannelOpen(b'RDPSND', False)
-        if RDPSND:
-            VCHandleValid = True
-            break
-        else:
-            time.sleep(1)
+    if not VCHandleValid:
+        RDPServer = OpenServer(b'localhost')
+        while True:
+            RDPSND = VirtualChannelOpen(b'RDPSND', False)
+            if RDPSND:
+                VCHandleValid = True
+                break
+            else:
+                time.sleep(1)
 
-    while True:
-        data = client_socket.recv(SIZE)
-        dataSize = len(data)
-        print(f'[+] {dataSize}bytes received')
-        hexdump(data, len(data))
-        
-        if not VirtualChannelWrite(RDPSND, data):
-            break
+    if not VirtualChannelWrite(RDPSND, data):
+        VCHandleValid = False
